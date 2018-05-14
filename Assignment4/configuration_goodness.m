78233036000f1d3e56929e12618546d9eb887bba
Xutong Zhao <zhaoxutongtong@gmail.com>
2018-05-14 17:27:55 -0600

    did assignment 4

--- Assignment4/configuration_goodness.m
+++ Assignment4/configuration_goodness.m
@@ -3,5 +3,8 @@ function G = configuration_goodness(rbm_w, visible_state, hidden_state)
 % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
 % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
 % This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
-    error('not yet implemented');
+	m1_ = hidden_state * visible_state' .* rbm_w;
+	[v_, h_] = size(m1_);
+	N_ = size(visible_state, 2);
+	G = sum(m1_(:)) / N_;
 end
